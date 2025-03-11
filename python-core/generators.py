198021d516482b63c5ef601644d316637351cf80
Arihant416 <arihantjain416@gmail.com>
2025-03-11 20:07:32 +0530

    A little on DSA end

--- python-core/generators.py
+++ python-core/generators.py
@@ -21,10 +21,27 @@ print(next(mygenerator))
 print(next(mygenerator))
 print(next(mygenerator))
 print(next(mygenerator))
-print(next(mygenerator))
+# print(next(mygenerator))
 print(
     next(mygenerator)
 )  # Since generator stops yielding values Exception : StopIteration is raised
 # yield pauses function execution and returns a value
 # when resumed execution starts right after the yield
 
+
+# Slightly on the advanced note:
+# Generators are memory-efficient
+# For example this is also similar to list-comprehension except it's ultra-memory efficient
+squares = (x*x for x in range(10))
+
+def fibo(n):
+    a , b = 0, 1
+    for _ in range(n):
+        yield a
+        a, b = b , a + b
+
+d = fibo(10)
+print(next(d))
+print(next(d))
+print(next(d))
+print(next(d))
